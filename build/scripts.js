0daa09cf53bb5bf889a04b8b6e6e9ac586f7f276
Kevin Docanto <kevin-teus@hotmail.com>
2022-08-23 16:12:52 -0400

    🔧 Changed character's name to Joe.

--- build/scripts.js
+++ build/scripts.js
@@ -1,14 +1,14 @@
 // Variables
-const fatMan = document.getElementById('fatMan');
+const joe = document.getElementById('joe');
 let weight = document.getElementById('weightNumber');
 
 // Makes character jump if space is pressed
 function jump(event) {
-  if (fatMan.classList != 'jump' && event.which === 32) {
-    fatMan.classList.add('jump');
+  if (joe.classList != 'jump' && event.which === 32) {
+    joe.classList.add('jump');
     weight.innerHTML = parseInt(weight.innerHTML) - 10;
     setTimeout(function () {
-      fatMan.classList.remove('jump');
+      joe.classList.remove('jump');
     }, 300);
   }
 }
