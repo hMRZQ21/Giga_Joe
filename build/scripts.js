fe993cfffe60a067b93e8dedd3cc51c2ee19f488
Kevin Docanto <kevin-teus@hotmail.com>
2022-08-23 15:38:15 -0400

    ✅ Weight counter lowers by jump.

--- build/scripts.js
+++ build/scripts.js
@@ -1,10 +1,12 @@
 // Variables
 const fatMan = document.getElementById('fatMan');
+let weight = document.getElementById('weightNumber');
 
 // Makes character jump if space is pressed
 function jump(event) {
   if (fatMan.classList != 'jump' && event.which === 32) {
     fatMan.classList.add('jump');
+    weight.innerHTML = parseInt(weight.innerHTML) - 10;
     setTimeout(function () {
       fatMan.classList.remove('jump');
     }, 300);
