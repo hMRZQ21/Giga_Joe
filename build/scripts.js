4b5c30451430167d2842ae358a548186e53914f1
Kevin Docanto <kevin-teus@hotmail.com>
2022-08-26 17:16:54 -0400

    🔥 Last commit i promise 2

--- build/scripts.js
+++ build/scripts.js
@@ -14,22 +14,19 @@ let audio = new Audio('audios/theme_song.mp3');
 audio.volume = 0.1;
 audio.loop = true;
 
+// Start Joe animation
+joe.classList.add('joeOverweightWalk');
 
 // Makes character jump if space is pressed
 function jump(event) {
   if (joe.classList != 'jump' && event.which === 32) {
+    joe.removeAttribute('class');
     joe.classList.add('jump');
-    joe.classList.remove('joeOverweightWalk');
     weight.innerHTML = parseInt(weight.innerHTML) - 5;
     setTimeout(function () {
       joe.classList.remove('jump');
-      joe.classList.add('joeOverweightWalk');
     }, 700);
   }
-}
-
-function joeOverweightAnimation() {
-  if(joe.classList != 'joeOverweightWalk') { joe.classList.add('joeOverweightWalk'); }
 }
 
 // Collision function
@@ -38,11 +35,15 @@ let collision = setInterval(() => {
   let joeTop = parseInt(window.getComputedStyle(joe).getPropertyValue('top'));
 
   //get Hamburger's current x position
-  let hamburgerLeft = parseInt( window.getComputedStyle(hamburger).getPropertyValue('left'));
+  let hamburgerLeft = parseInt(
+    window.getComputedStyle(hamburger).getPropertyValue('left')
+  );
 
-  let appleLeft = parseInt( window.getComputedStyle(apple).getPropertyValue('left'));
+  let appleLeft = parseInt(
+    window.getComputedStyle(apple).getPropertyValue('left')
+  );
   //Checking if weight under an amount and win
-  if (weight.innerHTML <= 150) {
+  if (weight.innerHTML == 100) {
     toggleWinLose(winDisplay);
     document
       .querySelector('.restart-btn2')
@@ -61,8 +62,9 @@ let collision = setInterval(() => {
           window.location.reload();
           return false;
         });
-    } 
-    else { weight.innerHTML = parseInt(weight.innerHTML) + 20; }
+    } else {
+      weight.innerHTML = parseInt(weight.innerHTML) + 20;
+    }
   }
   if (appleLeft < 60 && appleLeft > 0 && joeTop >= 180) {
     weight.innerHTML = parseInt(weight.innerHTML) - 15;
@@ -71,20 +73,36 @@ let collision = setInterval(() => {
 
 // Changes shape depending on Joe's weight
 let checkWeight = setInterval(() => {
-  if (weight.innerHTML > 190){ joe.style.backgroundImage = `url(${joeShapes[0]})`;}
-  if (weight.innerHTML <= 190){ joe.style.backgroundImage = `url(${joeShapes[1]})`;}
+  if (weight.innerHTML > 190 && !joe.classList.contains('jump')) {
+    joe.style.backgroundImage = `url(${joeShapes[0]})`;
+    joe.classList.add('joeOverweightWalk');
+    joe.classList.remove('joeBetterShapeWalk');
+    document.getElementById('game').style.animationDuration = '6s';
+    hamburger.style.animationDuration = '2s';
+    apple.style.animationDuration = '4.5s';
+    audio.playbackRate = 1;
+  }
+  if (weight.innerHTML <= 190 && !joe.classList.contains('jump')) {
+    joe.style.backgroundImage = `url(${joeShapes[1]})`;
+    joe.classList.add('joeBetterShapeWalk');
+    joe.classList.remove('joeOverweightWalk');
+    document.getElementById('game').style.animationDuration = '2s';
+    hamburger.style.animationDuration = '1s';
+    apple.style.animationDuration = '2s';
+    audio.playbackRate = 1.5;
+  }
 });
 
 // Event handlers
-toggle.addEventListener('click', function () { // Dark and light mode
+toggle.addEventListener('click', function () {
+  // Dark and light mode
   if (switchIcon.classList.contains('fa-moon')) {
     switchIcon.classList.remove('fa-moon');
     switchIcon.classList.add('fa-sun');
     document.body.style.backgroundColor = 'black';
     document.body.style.transition = 'background-color .5s linear';
     document.getElementById('command').style.color = 'white';
-  } 
-  else {
+  } else {
     switchIcon.classList.remove('fa-sun');
     switchIcon.classList.add('fa-moon');
     document.body.style.backgroundColor = 'white';
@@ -93,17 +111,22 @@ toggle.addEventListener('click', function () { // Dark and light mode
   }
 
   switchIcon.classList.add('toggle-animation');
-  setTimeout(() => { switchIcon.classList.remove('toggle-animation');}, 500);
+  setTimeout(() => {
+    switchIcon.classList.remove('toggle-animation');
+  }, 500);
 });
 
 document.addEventListener('keydown', function (event) {
-  if(event.repeat) return;
-  if(winDisplay.style.display == 'block' || gameOverDisplay.style.display == 'block')
+  if (event.repeat) return;
+  if (
+    winDisplay.style.display == 'block' ||
+    gameOverDisplay.style.display == 'block'
+  )
     return;
   jump(event);
 });
 
-function toggleWinLose(condition){ 
+function toggleWinLose(condition) {
   condition.style.display = 'block';
   document.getElementById('game').style.animationPlayState = 'paused';
   joe.style.animationPlayState = 'paused';
@@ -112,13 +135,11 @@ function toggleWinLose(condition){
   clearInterval(collision);
 }
 
-joeOverweightAnimation(); // Function calls
-
 // Play game
-let play_btn = document.querySelector(".play-btn")
-let play_btn_display = document.querySelector("#start-game")
-play_btn.addEventListener("click", function() {
-  document.body.classList.remove("preload");
+let play_btn = document.querySelector('.play-btn');
+let play_btn_display = document.querySelector('#start-game');
+play_btn.addEventListener('click', function () {
+  document.body.classList.remove('preload');
   audio.play();
   play_btn_display.style.display = 'none';
-})
+});
